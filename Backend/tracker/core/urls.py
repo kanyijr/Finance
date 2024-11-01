2c2f0dd126b231ab21ce0ddc2a2f2581dc5e50f4
Mwenda Kanyi <bobmwendaa@gmail.com>
2024-11-02 00:19:37 +0300

    Creating the react frontend

--- Backend/tracker/core/urls.py
+++ Backend/tracker/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [  
-    path("", views.index, name="home")
+    path("api/home/", views.index, name="home"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
